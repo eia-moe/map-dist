27aed4f7438c1a214963de3cbd68b7649d47de9a
Sokunthet Sok <kunthet@users.noreply.github.com>
2022-02-07 14:59:23 +0700

    auto commit

--- js/chunk-2d0ba6db.bf174676.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba6db"],{"36bd":function(n,e,t){"use strict";t.r(e);var o=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h1",[n._v("Chart by project category: Coming Soon...")])},c=[],r=t("2877"),a={},u=Object(r["a"])(a,o,c,!1,null,null,null);e["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d0ba6db.bf174676.js.map
